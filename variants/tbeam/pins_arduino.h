7158108d96e52c231b6662954d865826350de0c7
nasimovy <smeetswout@hotmail.com>
2025-04-11 11:32:29 +0000

    further changes

--- variants/tbeam/pins_arduino.h
+++ variants/tbeam/pins_arduino.h
@@ -7,6 +7,11 @@
 
 #define LED_BUILTIN LED_RED
 
+static const uint8_t SS = 18;
+static const uint8_t MOSI = 27;
+static const uint8_t MISO = 19;
+static const uint8_t SCK = 5;
+
 static const uint8_t TX = 12;
 static const uint8_t RX = 34;
 
